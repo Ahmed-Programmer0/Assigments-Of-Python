e787f6c496814edfa78b5c970988f7a81d71736e
Ahmed-Programmer0 <ahmedwaelforcollage@gmail.com>
2026-01-22 13:03:45 +0200

    This is The Fourth Assigment In Python

---
+++ Assigment_4.py
@@ -0,0 +1,7 @@
+# This File Is For The Fourth Assigment Of Python
+# It Is About Printing Variable's Values in One Sentence
+# The Author Is Ahmed Wael
+name = "Ahmed Wael"
+age = "21"
+country = "Egypt"
+print("\"Hello , My Name Is " + name + " And Iam " + age + " Years Old and I Live in " + country + "\"")
