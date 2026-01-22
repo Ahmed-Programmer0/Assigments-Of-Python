5265cf4e80a5767469fcdd6a65367c77fee8b3a2
Ahmed-Programmer0 <ahmedwaelforcollage@gmail.com>
2026-01-22 12:01:26 +0200

    This is The Third Assigment In Python

---
+++ Assigment_3.py
@@ -0,0 +1,8 @@
+# This Is The Third Assigment 
+# It Is About Printing Each Variable 
+name = "Ahmed Wael"
+age = "21"
+country = "Egypt"
+print("\"Name: " + name + "\"")
+print("\"Age: " + age + "\"")
+print("\"Country: " + country + "\"")
