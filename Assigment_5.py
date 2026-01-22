a83b8bb5076aedda56ae0d872f7df6b7c049764a
Ahmed-Programmer0 <ahmedwaelforcollage@gmail.com>
2026-01-22 13:09:28 +0200

    This is The Fifth Assigment In Elzero Web School Course of Python

---
+++ Assigment_5.py
@@ -0,0 +1,8 @@
+# This Is File For THe Assigment 5 From Elzero Web School Course
+# This Is Abouut Printing The Type For Each Variable Of These Varaiables below
+name = "Ahmed Wael"
+age = "21"
+country = "Egypt"
+print(type(name))
+print(type(age))
+print(type(country))
