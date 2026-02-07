ac7b752f5bd61829f740e1ff7b4a2c825743a3f8
Ahmed-Programmer0 <ahmedwaelforcollage@gmail.com>
2026-02-07 11:51:10 +0200

    Week 3 : Fifth Assigment Numbers

---
+++ Week3/Assigment5.py
@@ -0,0 +1,16 @@
+# 100 ? 115 = -15
+# 50 ? 30 = 1500
+# 21 ? 4 = 1
+# 110 ? 11 = 10
+# 97 ? 20 = 4
+# Put The operartor that will give you the result in above
+a = 100 - 115
+print(a)
+b = 50 * 30
+print(b) 
+c = 21 % 4
+print(c)
+d = 110 // 11
+print(d)
+e = 97 // 20
+print(e)
