32b573c35b5ceeb707bc29186f6b1d8f38b2c5d4
Ahmed-Programmer0 <ahmedwaelforcollage@gmail.com>
2026-02-09 08:13:43 +0200

    Week 3 : Fifth Assigment For Lists

---
+++ Week3/AssigmentListt5.py
@@ -0,0 +1,7 @@
+# Adding In the Front of Next List
+# And Adding In End Of Next List
+friends = ["Osama", "Ahmed", "Sayed"]
+friends.insert(0, "Nasser")
+print(friends)
+friends.append("Salem")
+print(friends)
