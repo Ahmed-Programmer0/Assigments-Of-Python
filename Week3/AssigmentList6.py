4e5b154c6739794c4ea3dcb27ec823438497aa0e
Ahmed-Programmer0 <ahmedwaelforcollage@gmail.com>
2026-02-09 08:20:26 +0200

    Week 3 : Sixth Assigment For Lists

---
+++ Week3/AssigmentList6.py
@@ -0,0 +1,8 @@
+# Deleting The First Two names In The List
+# In The Next List
+friends = ["Nasser", "Osama", "Ahmed", "Sayed", "Salem"]
+friends.pop(0)
+friends.pop(0)
+print(friends)
+friends.pop(-1)
+print(friends)
