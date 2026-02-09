2bb541775bd0ee652e910e1a586df0f6807fc808
Ahmed-Programmer0 <ahmedwaelforcollage@gmail.com>
2026-02-09 07:47:25 +0200

    Week 3 : Second Assigment For Lists

---
+++ Week3/AssigmentList2.py
@@ -0,0 +1,6 @@
+# The List For Printing (Names With Even indices and Names With Odd Indices)
+friends = ["Osama", "Ahmed", "Sayed", "Ali", "Mahmoud"]
+# The Even Names 
+print(friends[::2])
+# The Odd Names
+print(friends[1::2])
