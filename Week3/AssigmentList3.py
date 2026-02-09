11776a586c8f17d9b3087209ffd3e7cd9813d2f3
Ahmed-Programmer0 <ahmedwaelforcollage@gmail.com>
2026-02-09 08:01:31 +0200

    Week 3 : Third Assigment For Lists

---
+++ Week3/AssigmentList3.py
@@ -0,0 +1,6 @@
+# List For Names 
+# The Names that will taked from is Element with 1 2 3
+# And Names with will taked -1 -2 
+friends = ["Osama", "Ahmed", "Sayed", "Ali", "Mahmoud"]
+print(friends[1:4])
+print(friends[-2:])
